fe4ed70d3f26543d67d6774f2bff3c92fcfa792d
Parixit-Patel <parixit1303@gmail.com>
2025-12-26 20:33:20 +0530

    2 check even and odd using Bitwise and 3. print in number in binary format added

---
+++ 01_Basics/01_Bit_wise_operator/02_Check_ODD_EVEN_using_Bit_wise.c
@@ -0,0 +1,1 @@
+//2. WAP whether a number is ODD or EVEN using bitwise.
---
+++ 01_Basics/01_Bit_wise_operator/03_print_binary.c
@@ -0,0 +1,7 @@
+/* 3. Write a printbinary(int , int ) function consists of 2 integer variables. First one is the
+value of the variable and the second one is the size of the variable.
+Example:
+char x=5;
+printbinary(x,sizeof(x));
+output: 00000101
+*/
