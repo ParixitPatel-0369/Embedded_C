164901781091fc4e4d7a29f7b50a6be74fe808d0
Parixit-Patel <parixit1303@gmail.com>
2025-12-26 20:40:10 +0530

    2_check_even_odd using bitwise done.

--- 01_Basics/01_Bit_wise_operator/02_Check_ODD_EVEN_using_Bit_wise.c
+++ 01_Basics/01_Bit_wise_operator/02_Check_ODD_EVEN_using_Bit_wise.c
@@ -1,1 +1,21 @@
 //2. WAP whether a number is ODD or EVEN using bitwise.
+#include<stdio.h>
+#include<stdint.h>
+uint8_t check_odd(const uint32_t );
+int main(){
+    uint32_t num;
+    printf("Enter the number: ");
+    scanf("%u",&num);
+    if(check_odd(num))
+        printf("%u is ODD\n",num);
+    else
+        printf("%u is EVEN\n",num);
+    return 0;
+}
+uint8_t check_odd(const uint32_t num){
+    uint8_t odd;
+    if(num&1)
+        return 1;
+    else
+        return 0;
+}
